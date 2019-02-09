2848745d9d46ff1ba06e3e51b1a9fa42ff81dffa
shyuhei-kaneko <shyuhei0709@gmail.com>
2019-02-09 07:06:18 +0000

    Finish static pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,27 @@
+class StaticPagesController < ApplicationController
+
+  def home
+    # if logged_in?
+      # @micropost  = current_user.microposts.build
+      # 検索拡張機能として.search(params[:search])を追加 
+      # @feed_items = current_user.feed.paginate(page: params[:page]).search(params[:search])
+      # @user = User.find(current_user.id)
+      # redirect_to @user
+    # end
+  end
+
+  def link
+  end
+
+  def about
+  end
+
+  def contact
+  end
+  
+  def help
+  end
+
+  def about
+  end
+end
